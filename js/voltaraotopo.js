8dfe118c6a4948513d2ff1859c398fed41ee8037
stefannyss <stefanny128284@gmail.com>
2021-10-28 08:38:01 -0300

    stefanny

--- js/voltaraotopo.js
+++ js/voltaraotopo.js
@@ -3,5 +3,4 @@ btn.addEventListener('click', function () {
   window.scrollTo(0, 0)
 })
 
-/* define uma hash (ou id) para o comando btn, quando clicar no botão na pag html,
-ele abre um evento "click" e faz com que a tela volte ao topo com o Scrollto (0,0) "faz um scrol até o topo"*/
+/* ele abre um evento "click" e faz com que a tela volte ao topo com o Scrollto (0,0) "faz um scrol até o topo"*/
